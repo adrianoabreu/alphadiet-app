b0d35cc074580a44b3672855a1cc6cada5ed9c83
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2024-12-24 11:38:39 -0300

    Implementacao das entidades Pais, Estado, Cidade, Categoria e UnidadeComercial alem do seed do banco de dados

--- src/main/java/com/alphadiet/app/entities/Pais.java
+++ src/main/java/com/alphadiet/app/entities/Pais.java
@@ -1,5 +1,88 @@
 package com.alphadiet.app.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pais")
 public class Pais {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sigla;
+	private String descricao;
+	private Integer codigo;
+	
+	public Pais() {
+		
+	}
+
+	public Pais(Long id, String sigla, String descricao, Integer codigo) {
+		super();
+		this.id = id;
+		this.sigla = sigla;
+		this.descricao = descricao;
+		this.codigo = codigo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pais other = (Pais) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Pais [id=" + id + ", sigla=" + sigla + ", descricao=" + descricao + ", codigo=" + codigo + "]";
+	}
+	
 }
--- src/main/java/com/alphadiet/app/entities/UnidadeComercial.java
+++ src/main/java/com/alphadiet/app/entities/UnidadeComercial.java
@@ -1,5 +1,80 @@
 package com.alphadiet.app.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_unidade_comercial")
 public class UnidadeComercial {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sigla;
+	private String descricao;
+	
+	public UnidadeComercial() {
+		
+	}
+
+	public UnidadeComercial(Long id, String sigla, String descricao) {
+		super();
+		this.id = id;
+		this.sigla = sigla;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnidadeComercial other = (UnidadeComercial) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "UnidadeComercial [id=" + id + ", sigla=" + sigla + ", descricao=" + descricao + "]";
+	}
+	
+	
 }
